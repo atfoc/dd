cff300284b0f5b363c4ec2213cd43f788989a56b
Pedja Trifunov <trifunov.pedja@gmail.com>
2018-11-24 12:08:38 +0100

    Added main file

---
+++ main.ts
@@ -0,0 +1,13 @@
+import {BrowserWindow} from 'electron';
+import {app} from 'electron';
+
+let mainWindow : BrowserWindow | null = null;
+
+function main()
+{
+	mainWindow = new BrowserWindow();
+	mainWindow.show();
+}
+
+
+app.on('ready', main);
