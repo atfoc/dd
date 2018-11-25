c5ee036f06217f73035b178dd51bd963e2c42bb7
Pedja Trifunov <trifunov.pedja@gmail.com>
2018-11-25 14:11:57 +0100

    Added images to main window

--- src/components/MainWindow/MainWindow.tsx
+++ src/components/MainWindow/MainWindow.tsx
@@ -1,14 +1,20 @@
 import './style.css';
 import * as React from 'react';
 import {State, Props} from './State';
-import {setCategory, setResolution, toggleLoading} from './State';
+import {setCategory, setResolution, setLoading, addImages, clearImages} from './State';
+import {WallpaperCraftApi} from '../../utils/WallpaperCraftApi';
 
 import {Col, Container, Row} from "reactstrap";
-
 import {SideBar} from "../SideBar/SideBar";
+import {ImageShow} from '../ImageShow/ImageShow';
+
+import {Image} from "../../models/Image";
 
 class MainWindow extends React.Component<Props, State>
 {
+
+	private api:WallpaperCraftApi;
+	private onReloadImages:()=>void;
 
 	constructor(props:Props)
 	{
@@ -18,30 +24,73 @@ class MainWindow extends React.Component<Props, State>
 			{
 				category:null,
 				resolution:null,
-				loading:false
+				loading:false,
+				images:[]
 			};
 
+		this.api = WallpaperCraftApi.getInstance();
 
 		this.onCategoryChange = this.onCategoryChange.bind(this);
 		this.onResolutionChange = this.onResolutionChange.bind(this);
+		this.onImagesRecived = this.onImagesRecived.bind(this);
+		this.onReloadImages = this.onGetMoreImages.bind(this, 1);
 	}
 
 
+
 	/*View code*/
+	onGetMoreImages(page:number)
+	{
+		if(this.state.resolution && this.state.category)
+		{
+			this.api.getImages(this.state.resolution, this.state.category, page)
+				.then(this.onImagesRecived)
+				.catch(reason =>
+				{
+					console.log(reason);
+					this.setState(setLoading(false));
+				});
+		}
+	}
+
 	onCategoryChange(category: string):void
 	{
 		this.setState(setCategory(category));
-		this.setState(toggleLoading, ()=>setTimeout(()=>this.setState(toggleLoading),1000));
+		this.setState(setLoading(true));
+		this.setState(clearImages, this.onReloadImages);
 	}
 
 	onResolutionChange(resolution:string):void
 	{
 		this.setState(setResolution(resolution));
-		this.setState(toggleLoading, ()=>setTimeout(()=>this.setState(toggleLoading),1000));
+		this.setState(setLoading(true));
+		this.setState(clearImages, this.onReloadImages);
 
 	}
 
+	onImagesRecived(images:Array<Image>):void
+	{
+		this.setState(addImages(images));
+		this.setState(setLoading(false));
+	}
+
 	/*Render code*/
+
+	renderImages():React.ReactNode
+	{
+		const images = this.state.images.map(value => (
+				<Col  sm='12' md='6' lg='4' className='mt-2'>
+					<ImageShow image={value}/>
+				</Col>
+			));
+
+		return (
+			<React.Fragment>
+					{images}
+			</React.Fragment>
+		);
+	}
+
 	render(): React.ReactNode
 	{
 		const blocked = this.state.loading ? 'mainwindow-blocker' : '';
@@ -58,9 +107,10 @@ class MainWindow extends React.Component<Props, State>
 									 onResolutionChange={this.onResolutionChange}
 							/>
 						</Col>
-						<Col className='mainwindow-mainlayout'>
-							<h1>Resolution is: {this.state.resolution}</h1>
-							<h1>Category is {this.state.category}</h1>
+						<Col className='mainwindow-mainlayout' style={{width:'100vh'}}>
+								<Row className='mt-2'>
+									{this.renderImages()}
+								</Row>
 						</Col>
 					</Row>
 				</Container>
--- src/components/MainWindow/State.ts
+++ src/components/MainWindow/State.ts
@@ -1,8 +1,11 @@
+import {Image} from "../../models/Image";
+
 interface State
 {
 	category:string | null;
 	resolution:string | null;
 	loading:boolean;
+	images:Array<Image>;
 }
 
 type Props = {};
@@ -27,10 +30,31 @@ function setResolution(resolution:string):ReturnValueF<'resolution'>
 	};
 }
 
-function toggleLoading(state:Readonly<State>, props:Readonly<Props>):ReturnValue<'loading'>
+function setLoading(value:boolean):ReturnValueF<'loading'>
 {
-	return {loading:!state.loading};
+	return (state, props) => ({loading:value});
+
 }
 
 
-export {State, Props, setCategory, setResolution, toggleLoading};
+function addImages(images: Array<Image> | Image):ReturnValueF<'images'>
+{
+	return (state, props) =>
+	{
+		if(images instanceof Array)
+		{
+			return {images:[...state.images, ...images]};
+		}
+		else
+		{
+			return {images:[...state.images, images]};
+		}
+	};
+}
+
+function clearImages(state:Readonly<State>, props:Readonly<Props>):ReturnValue<'images'>
+{
+	return {images:[]};
+}
+
+export {State, Props, setCategory, setResolution, setLoading, addImages, clearImages};
