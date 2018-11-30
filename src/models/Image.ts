ca347e15202564d164f71ea7907d6f53a15263da
Pedja Trifunov <trifunov.pedja@gmail.com>
2018-11-30 15:16:29 +0100

    Added functions for run time type check of Image modal

--- src/models/Image.ts
+++ src/models/Image.ts
@@ -1,3 +1,4 @@
+
 class Image
 {
 	readonly name:string;
@@ -9,4 +10,21 @@ class Image
 	}
 }
 
-export {Image};
+function isImage(obj:any):obj is Image
+{
+	return isImageLike(obj) && obj instanceof Image;
+}
+
+function isImageLike(obj: any):obj is {[K in keyof Image]:Image[K]}
+{
+	if(obj.name && typeof(obj.name) !== 'string')
+	{
+		return false;
+	}
+	if(obj.format && typeof(obj.format) !== 'string')
+	{
+		return false;
+	}
+	return true;
+}
+export {Image, isImage, isImageLike};
